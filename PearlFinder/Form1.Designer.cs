8ee8a593f88cc657ac6300daa6e32407c82e7d03
jamesnogra <love.gerald@yahoo.com>
2017-08-19 15:15:56 +0800

    First push.

---
+++ PearlFinder/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace PearlFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.allPearlsPicbox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.patternPicbox = new System.Windows.Forms.PictureBox();
+            this.findButton = new System.Windows.Forms.Button();
+            this.testOutputPicBox = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.allPearlsPicbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.patternPicbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testOutputPicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // allPearlsPicbox
+            // 
+            this.allPearlsPicbox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.allPearlsPicbox.Location = new System.Drawing.Point(13, 13);
+            this.allPearlsPicbox.Name = "allPearlsPicbox";
+            this.allPearlsPicbox.Size = new System.Drawing.Size(500, 500);
+            this.allPearlsPicbox.TabIndex = 0;
+            this.allPearlsPicbox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(520, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Template";
+            // 
+            // patternPicbox
+            // 
+            this.patternPicbox.Location = new System.Drawing.Point(523, 30);
+            this.patternPicbox.Name = "patternPicbox";
+            this.patternPicbox.Size = new System.Drawing.Size(70, 70);
+            this.patternPicbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.patternPicbox.TabIndex = 2;
+            this.patternPicbox.TabStop = false;
+            // 
+            // findButton
+            // 
+            this.findButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.findButton.Location = new System.Drawing.Point(523, 225);
+            this.findButton.Name = "findButton";
+            this.findButton.Size = new System.Drawing.Size(70, 23);
+            this.findButton.TabIndex = 3;
+            this.findButton.Text = "Find";
+            this.findButton.UseVisualStyleBackColor = false;
+            this.findButton.Click += new System.EventHandler(this.findButton_Click);
+            // 
+            // testOutputPicBox
+            // 
+            this.testOutputPicBox.ErrorImage = null;
+            this.testOutputPicBox.InitialImage = null;
+            this.testOutputPicBox.Location = new System.Drawing.Point(523, 149);
+            this.testOutputPicBox.Name = "testOutputPicBox";
+            this.testOutputPicBox.Size = new System.Drawing.Size(70, 70);
+            this.testOutputPicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.testOutputPicBox.TabIndex = 4;
+            this.testOutputPicBox.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(520, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Scanning at:";
+            // 
+            // progressBar
+            // 
+            this.progressBar.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.progressBar.Location = new System.Drawing.Point(523, 254);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(70, 20);
+            this.progressBar.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 529);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.testOutputPicBox);
+            this.Controls.Add(this.findButton);
+            this.Controls.Add(this.patternPicbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.allPearlsPicbox);
+            this.Name = "Form1";
+            this.Text = "AAA Pearl Finder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.allPearlsPicbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.patternPicbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testOutputPicBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox allPearlsPicbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox patternPicbox;
+        private System.Windows.Forms.Button findButton;
+        private System.Windows.Forms.PictureBox testOutputPicBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ PearlFinder/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PearlFinder
+{
+    public partial class Form1 : Form
+    {
+        byte thresholdNum = 16;
+        Bitmap source, pattern;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void findButton_Click(object sender, EventArgs e)
+        {
+            int sad, sadThreshold = 10000;
+            int totalPattern;
+            List<Point> allMatches = new List<Point>();
+            Bitmap testOutput = new Bitmap(pattern.Width, pattern.Height);
+            float pctComplete = 0;
+
+            //for the source image
+            BitmapData sourceData = source.LockBits(new Rectangle(0, 0, source.Width, source.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            int sourceStride = sourceData.Stride;
+            System.IntPtr sourceScan = sourceData.Scan0;
+            //for the pattern image
+            BitmapData patternData = pattern.LockBits(new Rectangle(0, 0, pattern.Width, pattern.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            int patternStride = patternData.Stride;
+            System.IntPtr patternScan = patternData.Scan0;
+
+            progressBar.Visible = true;
+
+            unsafe
+            {
+                byte* s = (byte*)(void*)sourceScan;
+                int sourceOffset = sourceStride - source.Width * 3;
+                byte* p;
+                int patternOffset = patternStride - pattern.Width * 3;
+
+                for (int y = 0; y < source.Height - pattern.Height; ++y)
+                {
+                    for (int x = 0; x < source.Width; ++x)
+                    {
+                        //s[0], s[1], and s[2] for the source image
+                        //p[0], p[1], and p[2] for the pattern image
+                        sad = 0;
+                        totalPattern = 0;
+                        p = (byte*)(void*)patternScan;
+
+                        //for the moving scan image
+                        BitmapData scanData = testOutput.LockBits(new Rectangle(0, 0, testOutput.Width, testOutput.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+                        int scanStride = scanData.Stride;
+                        System.IntPtr scanScan = scanData.Scan0;
+                        byte* sc = (byte*)(void*)scanScan;
+                        int scanOffset = scanStride - testOutput.Width * 3;
+
+                        for (int i=0; i < pattern.Height; ++i)
+                        {
+                            for (int j=0; j < pattern.Width; ++j)
+                            {
+                                sad += Math.Abs(p[0] - s[0]);
+                                sc[0] = sc[1] = sc[2] = s[0]; //this is for the moving scanning image
+                                p += 3;
+                                s += 3;
+                                sc += 3;
+                                totalPattern += 3;
+                            }
+                            sc += scanOffset;
+                            p += patternOffset;
+                            s += source.Width * 3 + sourceOffset - pattern.Width * 3;
+                            totalPattern += source.Width * 3 + sourceOffset - pattern.Width * 3;
+                        }
+
+                        //display the moving scanning image
+                        testOutput.UnlockBits(scanData);
+                        testOutputPicBox.Image = testOutput;
+                        pctComplete = (y)*100 / (source.Height - pattern.Height);
+                        progressBar.Value = (int)pctComplete + 1;
+                        Application.DoEvents();
+
+                        if (sad < sadThreshold)
+                        {
+                            allMatches.Add(new Point(x, y));
+                            drawRectangleAt(x, y);
+                        }
+                        s += 3 - totalPattern;
+                    }
+                    s += sourceOffset;
+                }
+            }
+
+            source.UnlockBits(sourceData);
+            pattern.UnlockBits(patternData);
+            MessageBox.Show("Done scanning image! Found "+allMatches.Count+" matches.");
+            progressBar.Visible = false;
+        }
+
+        private void drawRectangleAt(int x, int y)
+        {
+            int sizeFactorX = allPearlsPicbox.Width / source.Width;
+            int sizeFactorY = allPearlsPicbox.Height / source.Height;
+            Graphics g = allPearlsPicbox.CreateGraphics();
+            Pen blackPen = new Pen(ColorTranslator.FromHtml("#33CC33"), 3);
+            g.DrawRectangle(blackPen, x * sizeFactorX, y * sizeFactorY, pattern.Width * sizeFactorX, pattern.Height * sizeFactorY);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            allPearlsPicbox.Image = Image.FromFile(@"AllPearls.png");
+            allPearlsPicbox.SizeMode = PictureBoxSizeMode.StretchImage;
+            patternPicbox.Image = Image.FromFile(@"PearlPattern.png");
+            patternPicbox.SizeMode = PictureBoxSizeMode.StretchImage;
+            source = new Bitmap(allPearlsPicbox.Image);
+            pattern = new Bitmap(patternPicbox.Image);
+            Threshold(source, thresholdNum);
+            testOutputPicBox.Image = pattern;
+            patternPicbox.Image = Image.FromFile(@"PearlPattern.png");
+            Threshold(pattern, thresholdNum);
+            progressBar.Visible = false;
+        }
+
+        public bool Threshold(Bitmap b, byte threshold)
+        {
+            // GDI+ still lies to us - the return format is BGR, NOT RGB.
+            BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            int stride = bmData.Stride;
+            System.IntPtr Scan0 = bmData.Scan0;
+
+            unsafe
+            {
+                byte* p = (byte*)(void*)Scan0;
+                int nOffset = stride - b.Width * 3;
+                byte red, green, blue;
+
+                for (int y = 0; y < b.Height; ++y)
+                {
+                    for (int x = 0; x < b.Width; ++x)
+                    {
+                        blue = p[0];
+                        green = p[1];
+                        red = p[2];
+                        if ((.333 * red + .333 * green + .334 * blue) > (255 - threshold))
+                        {
+                            p[0] = p[1] = p[2] = 255;
+                        }
+                        else
+                        {
+                            p[0] = p[1] = p[2] = 0;
+                        }
+                        p += 3;
+                    }
+                    p += nOffset;
+                }
+            }
+
+            b.UnlockBits(bmData);
+
+            return true;
+        }
+    }
+}
